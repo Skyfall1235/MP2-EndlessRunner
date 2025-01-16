6ee4a8e8dc3bef5d15b58a366baa3b8c68f721be
Wyatt Murray <113722636+Skyfall1235@users.noreply.github.com>
2025-01-16 18:17:10 -0500

    aded a couple base methods

--- root/MP2-EndlessRunner3D/Assets/Scripts/MoveableObject.cs
+++ root/MP2-EndlessRunner3D/Assets/Scripts/MoveableObject.cs
@@ -1,6 +1,26 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class MoveableObject : MonoBehaviour
 {
     //insert the abilities to move through phyisics here. will do later.
+
+    private Rigidbody objectRigidbody;
+    public Rigidbody ObjectRigidbody { get => objectRigidbody; set => objectRigidbody = value; }
+
+
+    public void MoveObject(Vector3 Direction, float speed)
+    {
+        objectRigidbody.AddForce(Direction * speed * Time.deltaTime); 
+    }
+
+    public void HaltObjectMovement()
+    {
+        objectRigidbody.linearVelocity = Vector3.zero;
+    }
+
+    public bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, 0.6f);
+    }
 }
