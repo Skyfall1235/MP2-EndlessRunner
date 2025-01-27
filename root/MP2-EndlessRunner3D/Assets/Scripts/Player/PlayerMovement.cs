b2d2affd3eae945a8987f69d4bc6ed9820491b78
Wyatt Murray <wyattmurray1235@gmail.com>
2025-01-27 16:56:34 -0500

    began Power Up Additions

--- root/MP2-EndlessRunner3D/Assets/Scripts/Player/PlayerMovement.cs
+++ root/MP2-EndlessRunner3D/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MoveableObject
@@ -12,12 +13,21 @@ public class PlayerMovement : MoveableObject
     private Rigidbody rb;
     public bool ReverseGravity;
     public bool TimeSlow;
+    public float PowerUpTime = 2f;
+    public float TimeSlowScale = 0.7f;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
-    
+    private void Update()
+    {
+        if(ReverseGravity)
+        {
+            rb.AddForce(Vector3.up * 9.81f, ForceMode.Acceleration);
+        }
+    }
+
     public void ForceJump()
     {
         //this is evoked via event so it just needs to execute
@@ -27,5 +37,27 @@ public class PlayerMovement : MoveableObject
             UpDirection = Vector3.down;
         }
         rb.linearVelocity = UpDirection * jumpForce;
+    }
+    public void ChangeGravity()
+    {
+        StartCoroutine(ChangeGravitySequence());
+    }
+    public void SlowTime()
+    {
+        StartCoroutine(SlowTimeSequence());
+    }
+
+    IEnumerator ChangeGravitySequence()
+    {
+        rb.useGravity = false;
+        yield return new WaitForSeconds(PowerUpTime);
+        rb.useGravity = true;
+    }
+    
+    IEnumerator SlowTimeSequence()
+    {
+        Time.timeScale = TimeSlowScale;
+        yield return new WaitForSeconds(PowerUpTime);
+        Time.timeScale = 1f;
     }
 }
